8c16019c4bfe0252f4344b9e66e9c5f96b2f03cb
unknown <mwalczyk2@gmail.com>
2020-01-30 14:18:03 -0800

    Adds shader class and arcball stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,250 @@
+#include <algorithm>
 #include <iostream>
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-void process_input(GLFWwindow* window)
+#include "shader.h"
+
+// Data that will be associated with the GLFW window
+struct InputData
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    bool imgui_active = false;
+};
+
+// Viewport and camera settings
+const uint32_t window_w = 800;
+const uint32_t window_h = 800;
+bool first_mouse = true;
+float last_x;
+float last_y;
+float zoom = 45.0f;
+glm::mat4 arcball_camera_matrix = glm::lookAt(glm::vec3{ 0.0f, 0.0f, 4.0f }, glm::vec3{ 0.0f }, glm::vec3{ 1.0f, 1.0f, 0.0f });
+glm::mat4 arcball_model_matrix = glm::mat4{ 1.0f };
+
+// Global settings
+// ...
+
+// Appearance settings
+ImVec4 clear_color = ImVec4(0.51f, 0.44f, 0.51f, 1.0f);
+
+InputData input_data;
+
+/**
+ * A function for handling scrolling.
+ */
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (zoom >= 1.0f && zoom <= 45.0f)
     {
-        glfwSetWindowShouldClose(window, true);
+        zoom -= yoffset;
+    }
+    if (zoom <= 1.0f)
+    {
+        zoom = 1.0f;
+    }
+    if (zoom >= 45.0f)
+    {
+        zoom = 45.0f;
     }
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+/**
+ * A function for handling key presses.
+ */
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    glViewport(0, 0, width, height);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        // Close the GLFW window
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (key == GLFW_KEY_H && action == GLFW_PRESS)
+    {
+        // Reset the arcball camera
+        arcball_camera_matrix = glm::lookAt(glm::vec3{ 6.0f, 0.0f, 0.0f }, glm::vec3{ 0.0f }, glm::vec3{ 1.0f, 1.0f, 0.0f });
+        arcball_model_matrix = glm::mat4{ 1.0f };
+    }
 }
 
-const uint32_t width = 800;
-const uint32_t height = 800;
+/**
+ * Get a normalized vector from the center of a virtual sphere centered at the origin to
+ * a point `point_on_sphere` on the virtual ball surface, such that `point_on_sphere`
+ * is aligned on screen's (x, y) coordinates.  If (x, y) is too far away from the
+ * sphere, return the nearest point on the virtual ball surface.
+ */
+glm::vec3 get_arcball_vector(int x, int y)
+{
+    auto point_on_sphere = glm::vec3{
+        1.0f * x / window_w * 2.0f - 1.0f,
+        1.0f * y / window_h * 2.0f - 1.0f,
+        0.0f
+    };
+
+    point_on_sphere.y = -point_on_sphere.y;
+
+    const float op_squared = point_on_sphere.x * point_on_sphere.x + point_on_sphere.y * point_on_sphere.y;
+
+    if (op_squared <= 1.0f * 1.0f)
+    {
+        // Pythagorean theorem
+        point_on_sphere.z = sqrt(1.0f * 1.0f - op_squared);
+    }
+    else
+    {
+        // Nearest point
+        point_on_sphere = glm::normalize(point_on_sphere);
+    }
+
+    return point_on_sphere;
+}
+
+/**
+ * Performs arcball camera calculations.
+ */
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    // First, check if the user is interacting with the ImGui interface - if they are,
+    // we don't want to process mouse events any further
+    auto input_data = static_cast<InputData*>(glfwGetWindowUserPointer(window));
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && !input_data->imgui_active)
+    {
+        if (first_mouse)
+        {
+            last_x = xpos;
+            last_y = ypos;
+            first_mouse = false;
+        }
+
+        if (xpos != last_x || ypos != last_y)
+        {
+            const float rotation_speed = 0.25f;
+
+            glm::vec3 va = get_arcball_vector(last_x, last_y);
+            glm::vec3 vb = get_arcball_vector(xpos, ypos);
+            const float angle = acos(std::min(1.0f, glm::dot(va, vb))) * rotation_speed;
+            const glm::vec3 axis_camera_coordinates = glm::cross(va, vb);
+
+            glm::mat3 camera_to_object = glm::inverse(glm::mat3(arcball_camera_matrix) * glm::mat3(arcball_model_matrix));
+
+            glm::vec3 axis_in_object_coord = camera_to_object * axis_camera_coordinates;
+
+            arcball_model_matrix = glm::rotate(arcball_model_matrix, glm::degrees(angle), axis_in_object_coord);
+
+            // Set last to current
+            last_x = xpos;
+            last_y = ypos;
+        }
+    }
+    else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+    {
+        last_x = xpos;
+        last_y = ypos;
+    }
+}
+
+/**
+ * Debug function that will be used internally by OpenGL to print out warnings, errors, etc.
+ */
+void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const* message, void const* user_param)
+{
+    auto const src_str = [source]() {
+        switch (source)
+        {
+        case GL_DEBUG_SOURCE_API: return "API";
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
+        case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
+        case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
+        case GL_DEBUG_SOURCE_OTHER: return "OTHER";
+        }
+    }();
+
+    auto const type_str = [type]() {
+        switch (type)
+        {
+        case GL_DEBUG_TYPE_ERROR: return "ERROR";
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
+        case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
+        case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
+        case GL_DEBUG_TYPE_MARKER: return "MARKER";
+        case GL_DEBUG_TYPE_OTHER: return "OTHER";
+        }
+    }();
+
+    auto const severity_str = [severity]() {
+        switch (severity) {
+        case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
+        case GL_DEBUG_SEVERITY_LOW: return "LOW";
+        case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
+        case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
+        }
+    }();
+
+    std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
+}
 
 int main()
 {
+    // Create and configure the GLFW window 
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    GLFWwindow* window = glfwCreateWindow(window_w, window_h, "Marching Cubes", nullptr, nullptr);
 
-    // Create GLFW window 
-    GLFWwindow* window = glfwCreateWindow(width, height, "ImGui Example", NULL, NULL);
-    if (window == NULL)
+    if (window == nullptr)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Load function pointers from GLAD
+    glfwMakeContextCurrent(window);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetWindowUserPointer(window, &input_data);
+
+    // Load function pointers from glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    const char* glsl_version = "#version 460";
+    // Initialize ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-    // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; 
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     ImGui::StyleColorsDark();
-
-    // Setup platform and renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui_ImplOpenGL3_Init("#version 460");
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Setup initial OpenGL state
+    {
+#if defined(_DEBUG)
+        // Debug logging
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(message_callback, nullptr);
+#endif
+        // Depth testing
+        glEnable(GL_DEPTH_TEST);
+
+        // Backface culling for optimization
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+    }
 
     while (!glfwWindowShouldClose(window))
     {
@@ -71,28 +255,21 @@ int main()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
         {
-            static float f = 0.0f;
-            static size_t counter = 0;
-
-            ImGui::Begin("Hello, world!");                          
-            ImGui::Text("This is some useful text.");              
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);           
+            ImGui::Begin("Settings");                             
             ImGui::ColorEdit3("clear color", (float*)&clear_color);
-            if (ImGui::Button("Button")) counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
-
-        // Finally, render
         ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
+
+        glViewport(0, 0, window_w, window_h);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Draw the ImGui window
+        {
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
 
         glfwSwapBuffers(window);
     }
